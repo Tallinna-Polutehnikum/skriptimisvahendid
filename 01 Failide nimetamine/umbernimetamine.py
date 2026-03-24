99eb41a4cb7a53f1be9639e621db567b7b8d294b
Sander <sander.luik@techno.ee>
2026-03-24 12:32:06 +0200

    Teine osa

--- 01 Failide nimetamine/umbernimetamine.py
+++ 01 Failide nimetamine/umbernimetamine.py
@@ -8,3 +8,19 @@ print("-" * 35)
 failid = os.listdir(kaust)   # tagastab nimekirja kaustas olevatest failidest
 for failinimi in failid:
     print(failinimi)
+
+print("Nimetan tühikud allkriipsudeks...")
+print("-" * 35)
+
+for failinimi in os.listdir(kaust):
+
+    if " " in failinimi:  # kontrolli, kas failinimi sisaldab tühikut
+
+        uus_nimi = failinimi.replace(" ", "_")  # asenda kõik tühikud
+
+        vana_tee = os.path.join(kaust, failinimi)  # täielik tee vanale failile
+        uus_tee  = os.path.join(kaust, uus_nimi)   # täielik tee uuele failile
+
+        os.rename(vana_tee, uus_tee)  # teeb ümbernimetamise
+
+        print(f"  {failinimi}  →  {uus_nimi}")
