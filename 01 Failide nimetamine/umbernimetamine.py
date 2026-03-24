a178c7e2f2807a0c55ba004850b71bd917c3846f
Sander <sander.luik@techno.ee>
2026-03-24 12:33:00 +0200

    Kolmas osa

--- 01 Failide nimetamine/umbernimetamine.py
+++ 01 Failide nimetamine/umbernimetamine.py
@@ -24,3 +24,23 @@ for failinimi in os.listdir(kaust):
         os.rename(vana_tee, uus_tee)  # teeb ümbernimetamise
 
         print(f"  {failinimi}  →  {uus_nimi}")
+
+
+print("Lisan eesliited failitüübi järgi...")
+print("-" * 35)
+
+for failinimi in os.listdir(kaust):
+
+    if failinimi.endswith(".jpg") and not failinimi.startswith("pilt_"):
+        uus_nimi = "pilt_" + failinimi
+
+    elif failinimi.endswith(".txt") and not failinimi.startswith("tekst_"):
+        uus_nimi = "tekst_" + failinimi
+
+    else:
+        continue  # see fail ei vaja muutmist, jätame vahele
+
+    vana_tee = os.path.join(kaust, failinimi)
+    uus_tee  = os.path.join(kaust, uus_nimi)
+    os.rename(vana_tee, uus_tee)
+    print(f"  {failinimi}  →  {uus_nimi}")
